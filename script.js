510e5e3ad764a2261193eed7911493a90a5fd942
Code Foxx <iamcodefoxx@gmail.com>
2021-07-15 15:34:43 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const mediaTags = document.querySelector('#media-tags');
+var input = document.querySelector('input');
+var songUpload = document.querySelector('#songUpload');
+
+songUpload.addEventListener("change", (event) => {
+    var files = event.target.files;
+    document.getElementById("src").setAttribute("src", URL.createObjectURL(files[0]));
+    document.getElementById("audio").load();
+})
+
+input.addEventListener("change", (event) => {
+  var file = event.target.files[0];
+  jsmediatags.read(file, {
+    onSuccess: function(tag) { 
+    const data = tag.tags.picture.data;
+    const format = tag.tags.picture.format;
+
+    let base64String = "";
+    for (let i = 0; i < data.length; i++) {
+        base64String += String.fromCharCode(data[i]);
+    }
+    document.getElementById("img").setAttribute('src', `data:${format};base64,${window.btoa(base64String)}`);
+    document.getElementById("img").style.display = 'block';
+    document.getElementById("artist").textContent = tag.tags.artist;
+    document.getElementById("title").textContent = tag.tags.title;
+    document.getElementById("album").textContent = tag.tags.album;
+    document.getElementById("genre").textContent = tag.tags.genre;
+  },
+  onError: function(error) {
+    console.log(error);
+    document.getElementById("img").setAttribute('src', '');
+    document.getElementById("img").style.display = 'none';
+    document.getElementById("artist").textContent = 'Unknown';
+    document.getElementById("title").textContent = 'Unknown';
+    document.getElementById("album").textContent = 'Unknown';
+    document.getElementById("genre").textContent = 'Unknown';
+  }
+  })
+})
+
